5687002507ebaa1af9ea72cbe0cb2fd182ea40da
sierra hughey <sierrahughey@mailbox.org>
2021-08-06 00:40:27 +0000

    finished lab

--- static/js/tradingCard.jsx
+++ static/js/tradingCard.jsx
@@ -1,53 +1,53 @@
-const tradingCardData = [
-  {
-    name: "Balloonicorn",
-    skill: "video games",
-    imgUrl: "/static/img/balloonicorn.jpg",
-    cardId: 1,
-  },
-  {
-    name: "Float",
-    skill: "baking pretzels",
-    imgUrl: "/static/img/float.jpg",
-    cardId: 2,
-  },
-  {
-    name: "Llambda",
-    skill: "knitting scarves",
-    imgUrl: "/static/img/llambda.jpg",
-    cardId: 3,
-  },
-  {
-    name: "Off-By-One",
-    skill: "climbing mountains",
-    imgUrl: "/static/img/off-by-one.jpeg",
-    cardId: 4,
-  },
-  {
-    name: "Seed.py",
-    skill: "making curry dishes",
-    imgUrl: "/static/img/seedpy.jpeg",
-    cardId: 5,
-  },
-  {
-    name: "Polymorphism",
-    skill: "costumes",
-    imgUrl: "/static/img/polymorphism.jpeg",
-    cardId: 6,
-  },
-  {
-    name: "Short Stack Overflow",
-    skill: "ocean animal trivia",
-    imgUrl: "/static/img/shortstack-overflow.jpeg",
-    cardId: 7,
-  },
-  {
-    name: "Merge",
-    skill: "bullet journaling",
-    imgUrl: "/static/img/merge.png",
-    cardId: 8,
-  },
-];
+// const tradingCardData = [
+//   {
+//     name: "Balloonicorn",
+//     skill: "video games",
+//     imgUrl: "/static/img/balloonicorn.jpg",
+//     cardId: 1,
+//   },
+//   {
+//     name: "Float",
+//     skill: "baking pretzels",
+//     imgUrl: "/static/img/float.jpg",
+//     cardId: 2,
+//   },
+//   {
+//     name: "Llambda",
+//     skill: "knitting scarves",
+//     imgUrl: "/static/img/llambda.jpg",
+//     cardId: 3,
+//   },
+//   {
+//     name: "Off-By-One",
+//     skill: "climbing mountains",
+//     imgUrl: "/static/img/off-by-one.jpeg",
+//     cardId: 4,
+//   },
+//   {
+//     name: "Seed.py",
+//     skill: "making curry dishes",
+//     imgUrl: "/static/img/seedpy.jpeg",
+//     cardId: 5,
+//   },
+//   {
+//     name: "Polymorphism",
+//     skill: "costumes",
+//     imgUrl: "/static/img/polymorphism.jpeg",
+//     cardId: 6,
+//   },
+//   {
+//     name: "Short Stack Overflow",
+//     skill: "ocean animal trivia",
+//     imgUrl: "/static/img/shortstack-overflow.jpeg",
+//     cardId: 7,
+//   },
+//   {
+//     name: "Merge",
+//     skill: "bullet journaling",
+//     imgUrl: "/static/img/merge.png",
+//     cardId: 8,
+//   },
+// ];
 
 function TradingCard(props) {
   return (
@@ -60,12 +60,22 @@ function TradingCard(props) {
 }
 
 function TradingCardContainer() {
+  const [cards, updateCards] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch('/cards.json')
+    .then((response) => response.json())
+    .then((data) => updateCards(data.cards));
+  }, [])
+
+  //const [cards, updateCards] = React.useState([floatCard]);
+
   const tradingCards = [];
 
-  for (const currentCard of tradingCardData) {
+  for (const currentCard of cards) {
     tradingCards.push(
       <TradingCard
-        key={currentCard.cardId}
+        key={currentCard.name}
         name={currentCard.name}
         skill={currentCard.skill}
         imgUrl={currentCard.imgUrl}
@@ -73,7 +83,10 @@ function TradingCardContainer() {
     );
   }
 
-  return <div className="grid">{tradingCards}</div>;
+  return (
+    <div>{tradingCards}</div>
+  );
+
 }
 
 ReactDOM.render(<TradingCardContainer />, document.getElementById("container"));
